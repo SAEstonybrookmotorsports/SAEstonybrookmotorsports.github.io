092412bea03802a3c5a42aa3cf5834353117aa55
CVRobo <ktaneja0910@gmail.com>
2025-07-09 00:38:43 +0530

    Configuration changes to make website work with pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import image from '@astrojs/image';
 
 export default defineConfig({
-  output: 'server', // Enable server-side rendering
+  output: 'static',
   site: 'https://CVRobo.github.io',
-  adapter: node({
-  mode: 'standalone',
   integrations: [image()],
-
-  }),
 });
