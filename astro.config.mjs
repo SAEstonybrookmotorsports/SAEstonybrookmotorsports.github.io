2e4f2e9d3f2e9cdc23e11a9c0b58d21f6bf11678
CVRobo <ktaneja0910@gmail.com>
2025-07-08 23:17:28 +0530

    hopefully this makes website work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import image from '@astrojs/image';
 
 export default defineConfig({
   output: 'server', // Enable server-side rendering
+  site: 'https://CVRobo.github.io',
   adapter: node({
-    mode: 'standalone',
+  mode: 'standalone',
+  integrations: [image()],
+
   }),
 });
