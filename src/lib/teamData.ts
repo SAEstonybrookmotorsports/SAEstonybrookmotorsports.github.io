9140f6ed5c3761b8937d0721e10bdc788a7caead
CVRobo <ktaneja0910@gmail.com>
2025-09-29 16:18:01 -0400

    team page overhaul

--- src/lib/teamData.ts
+++ src/lib/teamData.ts
@@ -14,17 +14,20 @@ import samanthaImg from '../assets/team/samantha-margulies.jpg';
 import nickImg from '../assets/hero.jpg';
 import mishaImg from '../assets/team/misha-zaslavsky.jpg';
 import vinnyImg from '../assets/team/vinny-blendell.jpg';
+import AshtonImg from '../assets/hero.jpg';
+import ShornImg from '../assets/hero.jpg';
 
 export interface TeamMember {
   name: string;
   image: ImageMetadata;
   role: string;
-  subsystem: string;
+  system: string;
   linkedin?: string;
   bio: string;
   funBio?: string;
   custom?: string;
   lead: boolean;
+  category: 'Faculty' | 'Eboard' | 'SystemLead' | 'Grad' | 'Member';
 }
 
 export const teamData: TeamMember[] = [
@@ -32,135 +35,178 @@ export const teamData: TeamMember[] = [
     name: 'Kushagra Taneja',
     image: kushagraImg,
     role: 'Software Lead',
-    subsystem: 'Software',
+    system: 'Software',
     linkedin: 'https://www.linkedin.com/in/kushagra-taneja/',
     bio: 'Website Developer / Data Acquisition Systems / CAN Bus',
     funBio: 'beep boop',
-    lead: true
+    lead: true,
+    category:'Member',
   },
   {
     name: 'Edward Cibu',
     image: edwardImg,
     role: 'Brakes Lead / Internal PR',
-    subsystem: 'Brakes',
+    system: 'Vehicle Dynamics',
     bio: 'insert professional bio here',
     funBio: 'car stopper and media manager',
-    lead: true
+    lead: true,
+    category:'Member',
   },
   {
     name: 'Brandon Chan',
     image: brandonImg,
     role: 'Suspension Lead',
-    subsystem: 'Suspension',
+    system: 'Vehicle Dynamics',
     linkedin: 'https://www.linkedin.com/in/brandon-chan-0467591ab/',
     bio: 'insert professional bio here',
     funBio: 'Make Car Boing Boing',
-    lead: true
+    lead: true,
+    category:'Member',
   },
   {
     name: 'Denys Tserkovniuk',
     image: denysImg,
     role: 'Driveshaft & Axles Lead',
-    subsystem: 'Drivetrain',
+    system: 'Drivetrain',
     bio: 'insert professional bio here',
-    lead: true
+    lead: true,
+    category:'Member',
   },
   {
     name: 'Erik Khachatryan',
     image: erikImg,
     role: 'CVT Lead',
-    subsystem: 'Transmission',
+    system: 'Drivetrain',
     bio: 'insert professional bio here',
-    lead: true
+    lead: true,
+    category:'Member',
   },
   {
     name: 'Arthur Siek',
     image: arthurImg,
     role: 'Vice President / CNC Lead',
-    subsystem: 'CNC',
+    system: 'CNC',
     bio: 'insert professional bio here',
     funBio: 'Run CNC',
-    lead: true
+    lead: true,
+    category:'Eboard',
   },
   {
     name: 'Hunter Guthy',
     image: hunterImg,
-    role: 'Frame',
-    subsystem: 'Chassis',
+    role: 'Frame Lead',
+    system: 'Chassis',
     linkedin: 'https://www.linkedin.com/in/hunterguthy/',
     bio: 'insert professional bio here',
     funBio: 'Partially appointed jig master',
     custom: '3rd year frame member',
-    lead: false
+    lead: false,
+    category:'Member',
   },
   {
     name: 'Julianna Wehmann',
     image: juliannaImg,
     role: 'President',
-    subsystem: 'Leadership',
+    system: 'Leadership',
     linkedin: 'https://www.linkedin.com/in/julianna-wehmann/',
     bio: 'insert professional bio here',
     funBio: 'Past PR Manager, Driveshaft/Axles Lead and Fabrication Lead.',
-    lead: true
+    lead: true,
+    category:'Eboard',
   },
   {
     name: 'Kevin Zhuo',
     image: kevinImg,
     role: 'Powertrain Lead / Head Machinist',
-    subsystem: 'Powertrain',
+    system: 'Powertrain',
     linkedin: 'https://www.linkedin.com/in/kevin-zhuo43/',
     bio: 'insert professional bio here',
     funBio: 'Past Gearbox Lead',
-    lead: true
+    lead: true,
+    category:'SystemLead',
   },
   {
     name: 'Ziqi Wang',
     image: ziqiImg,
     role: 'Gearbox Lead',
-    subsystem: 'Drivetrain',
+    system: 'Drivetrain',
     bio: 'insert professional bio here',
-    lead: true
+    lead: true,
+    category:'Member',
   },
   {
     name: 'Pasha Kusumo',
     image: pashaImg,
     role: 'Differential Co-Lead',
-    subsystem: 'Drivetrain',
+    system: 'Drivetrain',
     linkedin: 'https://www.linkedin.com/in/pashakusumo/',
     bio: 'insert professional bio here',
-    lead: true
+    lead: true,
+    category:'Member',
   },
   {
     name: 'Samantha Margulies',
     image: samanthaImg,
     role: 'Treasurer',
-    subsystem: 'Treasurer',
+    system: 'Treasurer',
     linkedin: 'https://www.linkedin.com/in/samantha-margulies',
     bio: 'insert professional bio here',
-    lead: false
+    lead: false,
+    category:'Eboard',
   },
   {
     name: 'Nick Clement',
     image: nickImg,
     role: 'Chassis Lead',
-    subsystem: 'Chassis',
+    system: 'Chassis',
     bio: 'insert professional bio here',
-    lead: true
+    lead: true,
+    category:'SystemLead',
   },
   {
     name: 'Misha Zaslavsky',
     image: mishaImg,
     role: 'Testing Lead',
-    subsystem: 'Testing',
+    system: 'Testing',
     bio: 'insert professional bio here',
-    lead: true
+    lead: true,
+    category:'Member',
   },
   {
     name: 'Vinny Blendell',
     image: vinnyImg,
     role: 'Composites Lead',
-    subsystem: 'Composites',
+    system: 'Chassis',
     bio: 'insert professional bio here',
-    lead: true
-  }
+    lead: true,
+    category:'Member',
+  },
+  {
+    name: 'Ashton Gomes',
+    image: AshtonImg,
+    role: 'Vehicle Dynamics Lead',
+    system: 'Vehicle Dynamics',
+    bio: 'insert professional bio here',
+    lead: true,
+    category:'SystemLead',
+  }, {
+    name: 'Richard Rennard',
+    image: AshtonImg,
+    role: 'Rickey',
+    system: 'Software',
+    bio: 'insert professional bio here',
+    lead: true,
+    category:'Member',
+  },
+  {
+    name: 'Shorn Grant',
+    image: ShornImg,
+    role: 'Electrical Lead',
+    system: 'Electrical',
+    bio: 'insert professional bio here',
+    lead: true,
+    category:'SystemLead',
+  },
+
+
 ];
